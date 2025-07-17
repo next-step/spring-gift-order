2341675e6c2ff4775f1e9a6fea9f3a5116790693
KIM TAE YUN <dltvmsskd@naver.com>
2025-07-17 12:46:20 +0900

    전남대 BE 김태윤 - 1단계 엔티티 매핑 (#132)
    * refactor(Item): Item 클래스를 JPA 엔티티로 전환
    
    * refactor(Item): ItemRepository JPA 기반으로 생성
    
    * feat(Item): saveItemm JPA 형식 추가
    
    * refactor:기존코드 임시 주석처리
    
    * feat(test): 저장하기 Test
    
    * feat(IItem): delete JPA 방식 추가
    
    * feat(Item): getItems JPA 방식 추가
    
    * feat(item): getAllItems JPA 방식 추가
    
    * feat(Item): findById JPA 방 방식 추가 및 컨트롤러 수정
    
    * feat(Item): findItemBy, updateItem 메서드 JPA 방식 추가
    
    * refactor(Item): controller 로직 수정
    
    * feat(User): UserRepositoryJPA 생성
    
    * refactor(User): User 클래스를 JPA 엔티티로 전환
    
    * feat(User): registerUser JPA 방식 추가
    
    * feat(User): findUserById 추가
    
    * feat(user): User 엔티티 클래스로 변경 및 updateUser JPA 방식 추가
    
    * feat(User): deleteUserById JPA 방식 추가
    
    * feat(User): getUserList / findUserById JPA 방식 추가
    
    * fix(entity): 엔티티 조건 수정 및 관련 메서드 수정
    
    * feat(wishList): wishListRepository 추가
    
    * feat(schema): 스키마 수정
    
    * refactor(wish): WishItem 클래스로 변환
    
    * refactor(wish): addWishItItem J JPA 방식 추가
    
    * refactor(wish): getItemList JPA 방식 추가
    
    * feat(wish): getWishItems JPA 방식 추가
    
    * refactor(wish): deleteWishItem JPA 방식 추가
    
    * refactor(wish) : updatedWishItem JPA 방식 추가
    
    * 엔티티 수정에 따른 메서드 수정
    
    * feat(test):ItemRepositoryTest 추가
    
    * docs: readme 작성
    
    * fix(User): registerUser 로직 수정
    
    * fix(User): deleteUser 로직 수정
    
    * fix(wish): addWishItem 로직수정
    
    * fix(wish): getItemList 반환값 수정
    
    * refactor:코드 리포메팅
    
    * fix(wishList):delete 메서드 삭제
    
    * fix(item):생성자 protected 수정
    
    * refactor:레포지토리 / 서비스 클래스명 변경
    
    * refactor(user): 메서드 명 변경 및 로그성 출력 제거
    
    * refactor(user,wishList):isValid user 엔티티에 추가
    
    * refactor(wish):getWishItems 메서드 삭제
    
    * refactor(item/wishList):update문 로직 수정
    
    * refactor(user): update 관련 메서드 수정
    
    * refactor(dto,controller): 반환 및 구조 변경
    
    * refactor(test): 테스트 코드 작성

--- src/main/java/gift/dto/wishListDto/AddWishItemDto.java
+++
@@ -1,7 +0,0 @@
-package gift.dto.wishListDto;
-
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-
-public record AddWishItemDto(@NotNull String name, @Min(0) Integer quantity) {
-}

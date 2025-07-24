0d983759a4b660168ddbbbaf79bdaaad1f3a99b3
uvrvuoon <0524yar@gmail.com>
2025-07-24 17:55:15 +0900

    feat: 카카오 로그인을 통해 엑세스 토큰을 받아오는 서비스 구현

---
+++ src/main/java/gift/service/KakaoLoginService.java
@@ -0,0 +1,44 @@
+package gift.service;
+
+import gift.dto.KakaoLoginResponse;
+import java.net.URI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class KakaoLoginService {
+
+    @Value("${kakao.client_id}")
+    private String clientId;
+
+    @Value("${kakao.redirect_uri}")
+    private String redirectUri;
+
+    public String getAccessToken(String code) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        var url = "https://kauth.kakao.com/oauth/token";
+        var headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+        var body = new LinkedMultiValueMap<String, String>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", clientId);
+        body.add("redirect_uri", redirectUri);
+        body.add("code", code);
+        var request = new RequestEntity<>(body, headers, HttpMethod.POST, URI.create(url));
+
+        ResponseEntity<KakaoLoginResponse> response = restTemplate.postForEntity(url, request, KakaoLoginResponse.class
+        );
+
+        return response.getBody().access_token();
+
+    }
+
+}

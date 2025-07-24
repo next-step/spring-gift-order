53fbc26d72223b71abe22c64f8b777975fd725ea
thdgmlwns1 <xhrl233@naver.com>
2025-07-24 13:40:36 +0900

    feat: 토큰 받아오기

---
+++ src/main/java/gift/controller/KakaoAuthController.java
@@ -0,0 +1,21 @@
+package gift.controller;
+
+import gift.dto.response.KaKaoTokenResponseDto;
+import gift.service.KaKaoAuthService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KakaoAuthController {
+    private final KaKaoAuthService kaKaoAuthService;
+
+    public KakaoAuthController(KaKaoAuthService kaKaoAuthService) {
+        this.kaKaoAuthService = kaKaoAuthService;
+    }
+
+    @GetMapping("/kakao")
+    public KaKaoTokenResponseDto kakaoToken(@RequestParam("code") String code) {
+        return kaKaoAuthService.requestToken(code);
+    }
+}
---
+++ src/main/java/gift/service/KaKaoAuthService.java
@@ -0,0 +1,38 @@
+package gift.service;
+
+import gift.dto.response.KaKaoTokenResponseDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
+
+
+@Service
+public class KaKaoAuthService {
+    private  RestClient restClient=RestClient.create();
+
+    @Value("${kakao.client-id}")
+    private String clientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String redirectUri;
+
+
+
+    public KaKaoTokenResponseDto requestToken(String code) {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", clientId);
+        body.add("redirect_uri", redirectUri);
+        body.add("code", code);
+        
+        return restClient.post()
+                .uri("https://kauth.kakao.com/oauth/token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(body)
+                .retrieve()
+                .body(KaKaoTokenResponseDto.class);
+    }
+}

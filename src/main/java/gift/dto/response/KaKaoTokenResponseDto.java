3b1927dc9bc24522e2a54b8856fb32b16ed9fc92
thdgmlwns1 <xhrl233@naver.com>
2025-07-25 01:38:16 +0900

    feat: record로 변환

--- src/main/java/gift/dto/response/KaKaoTokenResponseDto.java
+++ src/main/java/gift/dto/response/KaKaoTokenResponseDto.java
@@ -2,51 +2,24 @@ package gift.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class KaKaoTokenResponseDto {
+public record KaKaoTokenResponseDto(
 
-    @JsonProperty("token_type")
-    private String tokenType;
+        @JsonProperty("token_type")
+        String tokenType,
 
-    @JsonProperty("access_token")
-    private String accessToken;
+        @JsonProperty("access_token")
+        String accessToken,
 
-    @JsonProperty("expires_in")
-    private int expiresIn;
+        @JsonProperty("expires_in")
+        int expiresIn,
 
-    @JsonProperty("refresh_token")
-    private String refreshToken;
+        @JsonProperty("refresh_token")
+        String refreshToken,
 
-    @JsonProperty("refresh_token_expires_in")
-    private int refreshTokenExpiresIn;
+        @JsonProperty("refresh_token_expires_in")
+        int refreshTokenExpiresIn,
 
-    @JsonProperty("scope")
-    private String scope;
+        @JsonProperty("scope")
+        String scope
 
-
-
-    public String getTokenType() {
-        return tokenType;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public String getRefreshToken() {
-        return refreshToken;
-    }
-
-    public int getExpiresIn() {
-        return expiresIn;
-    }
-
-    public String getScope() {
-        return scope;
-    }
-
-    public int getRefreshTokenExpiresIn() {
-        return refreshTokenExpiresIn;
-    }
-}
-
-
+) {}

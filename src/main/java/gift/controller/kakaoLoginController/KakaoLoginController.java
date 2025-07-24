2e7c4f67ff4f02739b31e83caeed17a75e128ddb
xodbs126 <dltvmsskd@naver.com>
2025-07-24 22:36:08 +0900

    refactor(login): 인가코드 넘겨주는 로직

--- src/main/java/gift/controller/kakaoLoginController/KakaoLoginController.java
+++ src/main/java/gift/controller/kakaoLoginController/KakaoLoginController.java
@@ -24,13 +24,21 @@ public class KakaoLoginController {
     @Value("${kakao.redirect_uri}")
     private String redirectUri;
 
+    /***
+     * 지금 전체적 흐름
+     * 1. http://localhost:8080/login/page 접속
+     * 2. https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri로 자동 리다이렉트 -> 이게 카카오서버에서 인가코드 생성해줌
+     */
     @GetMapping("/login/page")
     public ResponseEntity<Void> redirectToKakao() {
         String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri;
         return ResponseEntity.status(302).header("Location", location).build();
     }
 
-    @GetMapping("/callback")
+    /***
+     * 어차피 http://localhost:8080/?code=... 로 리다이렉트 해주니까 매핑을 없애주면 될 거 같은데
+     */
+    @GetMapping
     public ResponseEntity<?> callback(@RequestParam("code") String code) {
         String accessToken = kakaoService.getAccessTokenFromKakao(code);
         System.out.println(accessToken);

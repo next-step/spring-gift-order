a56e72b7c7cc2f678eacc95790dc2ce7473a4637
raminKim <151427064+raminkim@users.noreply.github.com>
2025-07-24 16:40:47 +0900

    Refactor: RestTemplate → RestClient로 마이그레이션 및 KakaoTokenFetchException 추가

--- src/main/java/gift/client/KakaoClient.java
+++ src/main/java/gift/client/KakaoClient.java
@@ -1,14 +1,19 @@
 package gift.client;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import gift.config.KakaoProperties;
+import gift.dto.login.KakaoErrorDto;
 import gift.dto.login.KakaoProfileDto;
 import gift.dto.login.KakaoTokenDto;
+import gift.exception.KakaoTokenFetchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientResponseException;
 
 @Component
 public class KakaoClient {
@@ -17,8 +22,11 @@ public class KakaoClient {
 
     private final RestClient restClient;
 
-    public KakaoClient(KakaoProperties kakaoProperties) {
+    private final ObjectMapper objectMapper;
+
+    public KakaoClient(KakaoProperties kakaoProperties, ObjectMapper objectMapper) {
         this.kakaoProperties = kakaoProperties;
+        this.objectMapper = objectMapper;
         this.restClient = RestClient.create();
     }
 
@@ -35,14 +43,28 @@ public class KakaoClient {
         body.add("redirect_uri", kakaoProperties.redirectUri());
         body.add("code", code);
 
-        ResponseEntity<KakaoTokenDto> response = restClient.post()
-            .uri(baseUrl)
-            .headers(h -> h.addAll(headers))
-            .body(body)
-            .retrieve()
-            .toEntity(KakaoTokenDto.class);
+        try {
+            ResponseEntity<KakaoTokenDto> response = restClient.post()
+                .uri(baseUrl)
+                .headers(h -> h.addAll(headers))
+                .body(body)
+                .retrieve()
+                .toEntity(KakaoTokenDto.class);
 
-        return response.getBody();
+            return response.getBody();
+
+        } catch (RestClientResponseException exception) {
+            String json = exception.getResponseBodyAsString();
+            KakaoErrorDto errorDto = null;
+            try {
+                errorDto = objectMapper.readValue(json,
+                    KakaoErrorDto.class);
+            } catch (JsonProcessingException e) {
+                throw new KakaoTokenFetchException("카카오 오류 응답에 대한 파싱을 실패하였습니다.");
+            }
+
+            throw new KakaoTokenFetchException(errorDto.code(), errorDto.message());
+        }
     }
 
     public KakaoProfileDto fetchProfile(String token) {

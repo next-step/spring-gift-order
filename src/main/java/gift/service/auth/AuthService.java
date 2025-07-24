e597a9392372e8acda47fafd63c469e257b8a51d
Swallow Lee <lmh157@outlook.kr>
2025-07-25 00:13:01 +0900

    feat(service) : kakaoLogin 서비스 로직 구현
    - 로그인 중 에러 발생 시 error와 error_description을 읽어 처리(query paramenter)

--- src/main/java/gift/service/auth/AuthService.java
+++ src/main/java/gift/service/auth/AuthService.java
@@ -6,5 +6,6 @@ import java.util.Set;
 
 public interface AuthService {
     String login(String email, String password);
+    String kakaoLogin(String code, String error, String errorDescription);
     String signup(String email, String password, Set<UserRole> roles);
 }
--- src/main/java/gift/service/auth/AuthServiceImpl.java
+++ src/main/java/gift/service/auth/AuthServiceImpl.java
@@ -1,10 +1,12 @@
 package gift.service.auth;
 
+import gift.common.exception.KakaoAuthorizationException;
 import gift.common.exception.UnauthorizedException;
 import gift.common.util.PasswordEncoder;
 import gift.common.util.TokenProvider;
 import gift.entity.User;
 import gift.entity.type.UserRole;
+import gift.external.KakaoTokenClient;
 import gift.service.user.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,15 +17,18 @@ import java.util.Set;
 @Service
 public class AuthServiceImpl implements AuthService {
     private final UserService userService;
+    private final KakaoTokenClient kakaoOauth2Client;
     private final TokenProvider tokenProvider;
     private final PasswordEncoder passwordEncoder;
 
     public AuthServiceImpl(
             UserService userService,
+            KakaoTokenClient kakaoOauth2Client,
             TokenProvider tokenProvider,
             PasswordEncoder passwordEncoder
         ) {
         this.userService = userService;
+        this.kakaoOauth2Client = kakaoOauth2Client;
         this.tokenProvider = tokenProvider;
         this.passwordEncoder = passwordEncoder;
 
@@ -42,6 +47,15 @@ public class AuthServiceImpl implements AuthService {
             throw new UnauthorizedException("이메일 또는 비밀번호가 일치하지 않습니다.");
         }
         return tokenProvider.generateToken(user.getId(), user.getUserRoles());
+    }
+
+    @Override
+    public String kakaoLogin(String code, String error, String errorDescription) {
+        if (code == null || code.isBlank()) {
+            throw new KakaoAuthorizationException(error, errorDescription);
+        }
+        var tokenResponse = kakaoOauth2Client.getTokenResponse(code);
+        return tokenResponse.accessToken();
     }
 
     @Override

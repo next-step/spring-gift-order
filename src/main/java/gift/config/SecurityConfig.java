410ce546a160376545c7f62ad8302fc053015e77
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-07-26 05:54:17 +0900

    feat : 카카오 로그인 콜백 및 에러 페이지 접근 허용 추가

--- src/main/java/gift/config/SecurityConfig.java
+++ src/main/java/gift/config/SecurityConfig.java
@@ -35,11 +35,13 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "oauth/callback/kakao/**",
                                 "/api/members/register",
                                 "/api/members/login",
                                 "/h2-console/**",
                                 "/admin/**",
-                                "/css/**"
+                                "/css/**",
+                                "/error/**"
                         ).permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/products").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()

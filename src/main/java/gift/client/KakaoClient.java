fb5c2ad2fb0f7fee6cb01284119555945c84b413
raminKim <151427064+raminkim@users.noreply.github.com>
2025-07-25 21:25:42 +0900

    Refactor: RestControllerAdvice의 예외 분기 처리를 RestClient의 Error Handling 방식으로 변경

--- src/main/java/gift/client/KakaoClient.java
+++ src/main/java/gift/client/KakaoClient.java
@@ -1,19 +1,19 @@
 package gift.client;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gift.config.KakaoProperties;
 import gift.dto.login.KakaoErrorDto;
 import gift.dto.login.KakaoProfileDto;
 import gift.dto.login.KakaoTokenDto;
 import gift.exception.KakaoTokenFetchException;
+import java.io.IOException;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestClientResponseException;
 
 @Component
 public class KakaoClient {
@@ -41,28 +41,23 @@ public class KakaoClient {
         body.add("redirect_uri", kakaoProperties.redirectUri());
         body.add("code", code);
 
-        try {
-            ResponseEntity<KakaoTokenDto> response = restClient.post()
-                .uri(kakaoProperties.tokenUri())
-                .headers(h -> h.addAll(headers))
-                .body(body)
-                .retrieve()
-                .toEntity(KakaoTokenDto.class);
+        ResponseEntity<KakaoTokenDto> response = restClient.post()
+            .uri(kakaoProperties.tokenUri())
+            .headers(h -> h.addAll(headers))
+            .body(body)
+            .retrieve()
+            .onStatus(HttpStatusCode::isError, (request, httpResponse) -> {
+                try {
+                    KakaoErrorDto errorDto = objectMapper.readValue(httpResponse.getBody(),
+                        KakaoErrorDto.class);
+                    throw new KakaoTokenFetchException(errorDto.code(), errorDto.message());
+                } catch (IOException exception) {
+                    throw new KakaoTokenFetchException("카카오 오류 응답을 파싱하지 못하였습니다.");
+                }
+            })
+            .toEntity(KakaoTokenDto.class);
 
-            return response.getBody();
-
-        } catch (RestClientResponseException exception) {
-            String json = exception.getResponseBodyAsString();
-            KakaoErrorDto errorDto = null;
-            try {
-                errorDto = objectMapper.readValue(json,
-                    KakaoErrorDto.class);
-            } catch (JsonProcessingException e) {
-                throw new KakaoTokenFetchException("카카오 오류 응답에 대한 파싱을 실패하였습니다.");
-            }
-
-            throw new KakaoTokenFetchException(errorDto.code(), errorDto.message());
-        }
+        return response.getBody();
     }
 
     public KakaoProfileDto fetchProfile(String token) {

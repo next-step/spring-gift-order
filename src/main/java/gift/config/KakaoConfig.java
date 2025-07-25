db6c0cf4bacecbff2ceb29e72e87e7c2c552c429
wrd1stProgrammer <kicoa24@gmail.com>
2025-07-25 18:15:09 +0900

    feat : 빈 객체 생성

---
+++ src/main/java/gift/config/KakaoConfig.java
@@ -0,0 +1,10 @@
+package gift.config;
+
+import gift.auth.KakaoProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(KakaoProperties.class)
+public class KakaoConfig {
+}

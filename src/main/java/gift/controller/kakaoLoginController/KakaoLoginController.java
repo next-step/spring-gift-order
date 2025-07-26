0537c0da08a4364d86c12b43cf4916ecf8379351
xodbs126 <dltvmsskd@naver.com>
2025-07-26 10:47:37 +0900

    refactor(login): RestTemplate -> RestClient 변경

--- src/main/java/gift/controller/kakaoLoginController/KakaoLoginController.java
+++ src/main/java/gift/controller/kakaoLoginController/KakaoLoginController.java
@@ -1,43 +1,30 @@
 package gift.controller.kakaoLoginController;
 
-
+import gift.config.KakaoProperties;
 import gift.service.kakaoService.KakaoService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class KakaoLoginController {
 
     private final KakaoService kakaoService;
+    private final KakaoProperties kakaoProperties;
 
-    public KakaoLoginController(KakaoService kakaoService) {
+    public KakaoLoginController(KakaoService kakaoService, KakaoProperties kakaoProperties) {
         this.kakaoService = kakaoService;
+        this.kakaoProperties = kakaoProperties;
     }
 
-    @Value("${kakao.client_id}")
-    private String clientId;
 
-    @Value("${kakao.redirect_uri}")
-    private String redirectUri;
-
-    /***
-     * 지금 전체적 흐름
-     * 1. http://localhost:8080/login/page 접속
-     * 2. https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri로 자동 리다이렉트 -> 이게 카카오서버에서 인가코드 생성해줌
-     */
     @GetMapping("/login/page")
     public ResponseEntity<Void> redirectToKakao() {
-        String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri;
+        String location = "https://kauth.kakao.com/oauth/authorize" + "?response_type=code" + "&client_id=" + kakaoProperties.clientId() + "&redirect_uri=" + kakaoProperties.redirectUri();
+
         return ResponseEntity.status(302).header("Location", location).build();
     }
 
-    /***
-     * 어차피 http://localhost:8080/?code=... 로 리다이렉트 해주니까 매핑을 없애주면 될 거 같은데
-     */
     @GetMapping
     public ResponseEntity<?> callback(@RequestParam("code") String code) {
         String accessToken = kakaoService.getAccessTokenFromKakao(code);
--- src/main/java/gift/service/kakaoService/KakaoServiceImpl.java
+++ src/main/java/gift/service/kakaoService/KakaoServiceImpl.java
@@ -1,52 +1,34 @@
 package gift.service.kakaoService;
 
+import gift.config.KakaoProperties;
 import gift.dto.KakaoTokenResponseDto;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
-import java.net.URI;
+import java.util.Map;
 
 @Service
 public class KakaoServiceImpl implements KakaoService {
 
-    private final String clientId;
-    private final String redirectUri;
+    private final KakaoProperties kakaoProperties;
 
-    public KakaoServiceImpl(@Value("${kakao.client_id}") String clientId, @Value("${kakao.redirect_uri}") String redirectUri) {
-        this.clientId = clientId;
-        this.redirectUri = redirectUri;
+    public KakaoServiceImpl(KakaoProperties kakaoProperties) {
+        this.kakaoProperties = kakaoProperties;
     }
 
     @Override
     public String getAccessTokenFromKakao(String authorizationCode) {
+        RestClient client = RestClient.create();
 
-        String url = "https://kauth.kakao.com/oauth/token"; // POST 요청 시, 토큰 발급 URL
+        Map<String, String> form = Map.of("grant_type", "authorization_code", "client_id", kakaoProperties.clientId(), "redirect_uri", kakaoProperties.redirectUri(), "code", authorizationCode);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED); // 카카오 DEVELOP 기준
+        KakaoTokenResponseDto responseBody = client.post().uri("https://kauth.kakao.com/oauth/token").contentType(MediaType.APPLICATION_FORM_URLENCODED).body(form).retrieve().body(KakaoTokenResponseDto.class);
 
-        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        body.add("grant_type", "authorization_code"); // 인가코드로 고정
-        body.add("client_id", clientId); // 앱 REST API 키
-        body.add("redirect_uri", redirectUri); // 토큰 발급 시 응답으로 받은 refresh_token
-        body.add("code", authorizationCode); // 인가 코드
-
-        RequestEntity<MultiValueMap<String, String>> request = new RequestEntity<>(body, headers, HttpMethod.POST, URI.create(url));
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<KakaoTokenResponseDto> response = restTemplate.exchange(request, KakaoTokenResponseDto.class); // 요청 보냄
-
-        KakaoTokenResponseDto responseBody = response.getBody();
-        if (responseBody == null || responseBody.getAccessToken() == null) {
+        if (responseBody == null || responseBody.accessToken() == null) {
             throw new RuntimeException("카카오 토큰 요청 실패");
         }
-        System.out.println(responseBody.getRefreshToken());
-        System.out.println(responseBody.getRefreshTokenExpiresIn());
-        System.out.println(responseBody.getExpiresIn());
-        return responseBody.getAccessToken();
+
+        return responseBody.accessToken();
     }
 }

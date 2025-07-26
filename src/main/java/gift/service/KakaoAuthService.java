e299ed2c92b4e7894605f758cc4e7c17b9e0fada
sanghyeon1225 <oopt1234@naver.com>
2025-07-26 13:57:21 +0900

    feat: 4xx, 5xx, 서버 연결, 응답 시간 초과 시 에러 핸들링 구현

--- src/main/java/gift/service/KakaoAuthService.java
+++ src/main/java/gift/service/KakaoAuthService.java
@@ -3,21 +3,30 @@ package gift.service;
 import gift.config.KakaoProperties;
 import gift.dto.KakaoTokenRequestDto;
 import gift.dto.KakaoTokenResponseDto;
-import org.springframework.core.ParameterizedTypeReference;
+import gift.exception.KakaoAuthenticationException;
+import gift.exception.KakaoConnectionException;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClient;
 
 @Service
 public class KakaoAuthService {
+    private static final int CONNECTION_TIMEOUT_SECONDS = 3000;
+    private static final int READ_TIMEOUT_SECONDS = 3000;
     private static final String KAKAO_AUTH_URL = "https://kauth.kakao.com";
     private final RestClient restClient;
     private final KakaoProperties kakaoProperties;
 
     public KakaoAuthService(KakaoProperties kakaoProperties) {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(CONNECTION_TIMEOUT_SECONDS);
+        requestFactory.setReadTimeout(READ_TIMEOUT_SECONDS);
+
         this.restClient = RestClient.builder()
+                .requestFactory(requestFactory)
                 .baseUrl(KAKAO_AUTH_URL)
                 .build();
         this.kakaoProperties = kakaoProperties;
@@ -30,14 +39,23 @@ public class KakaoAuthService {
                 authorizeCode
         );
 
-        KakaoTokenResponseDto response = restClient.post()
-                .uri("/oauth/token")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(body.dtoToFormData())
-                .retrieve()
-                .body(new ParameterizedTypeReference<>() {
-                });
+        try {
+            KakaoTokenResponseDto response = restClient.post()
+                    .uri("/oauth/token")
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .body(body.dtoToFormData())
+                    .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError, (request, res) -> {
+                        throw new KakaoAuthenticationException("사용자의 카카오 인증에 실패했습니다. 응답 코드: " + res.getStatusCode());
+                    })
+                    .onStatus(HttpStatusCode::is5xxServerError, (request, res) -> {
+                        throw new KakaoConnectionException("카카오 서버에 오류가 발생했습니다. 응답 코드: " + res.getStatusCode());
+                    })
+                    .body(KakaoTokenResponseDto.class);
 
-        return response.accessToken();
+            return response.accessToken();
+        } catch (ResourceAccessException e) {
+            throw new KakaoConnectionException("카카오 서버와 연결/응답 시간이 초과했습니다.", e);
+        }
     }
 }

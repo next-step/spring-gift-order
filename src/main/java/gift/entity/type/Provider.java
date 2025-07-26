3f4f7fe1891819471eb9b8074b62b60605155971
Swallow Lee <lmh157@outlook.kr>
2025-07-27 00:37:24 +0900

    refactor: Provider LOCAL -> EMAIL로 수정
    - init-db에 수정 내용 반영

--- src/main/java/gift/entity/type/Provider.java
+++ src/main/java/gift/entity/type/Provider.java
@@ -1,5 +1,5 @@
 package gift.entity.type;
 
 public enum Provider {
-    LOCAL, KAKAO
+    EMAIL, KAKAO
 }
--- src/main/resources/sql/init-db.sql
+++ src/main/resources/sql/init-db.sql
@@ -79,8 +79,10 @@ INSERT INTO roles (name) VALUES
 
 -- test 어드민 사용자 test@test.com qwerty1234@
 INSERT INTO users (email, password, client_id, provider) VALUES
-    ('test@test.com', '1469f57c482317fba59bb34d16c10b0f5116e64c2201e430a70cc16a34a6a785', NULL, 'LOCAL');
+    ('test@test.com', '1469f57c482317fba59bb34d16c10b0f5116e64c2201e430a70cc16a34a6a785', NULL, 'EMAIL'),
+    (null, null, 'd4a76f830d09f0064d77af2700955dc600dd7d1b3c9ba3ecc44a99fbb94ef54f', 'KAKAO');
 
 -- 관리자 계정 부여
 INSERT INTO user_roles(user_id, role_name) VALUES
-    (1, 'ROLE_ADMIN');
+    (1, 'ROLE_ADMIN'),
+    (2, 'ROLE_USER');

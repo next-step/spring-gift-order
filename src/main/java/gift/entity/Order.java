29c73fd5dbf296874c935dcc86362c72d8eeb4a3
thdgmlwns1 <xhrl233@naver.com>
2025-07-27 00:40:20 +0900

    feat:Order entity 생성

---
+++ src/main/java/gift/entity/Order.java
@@ -0,0 +1,44 @@
+package gift.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+
+    private String message;
+
+    private LocalDateTime orderDateTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_id", nullable = false)
+    private ProductOption option;
+
+    protected Order() {}
+
+    public Order(Member member, ProductOption option, int quantity, String message) {
+        this.member = member;
+        this.option = option;
+        this.quantity = quantity;
+        this.message = message;
+        this.orderDateTime = LocalDateTime.now();
+    }
+
+    public Long getId() { return id; }
+    public int getQuantity() { return quantity; }
+    public String getMessage() { return message; }
+    public LocalDateTime getOrderDateTime() { return orderDateTime; }
+    public Member getMember() { return member; }
+    public ProductOption getOption() { return option; }
+}
+

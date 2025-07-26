5d711afea4a1496a997e782962f498217a1aaefd
thdgmlwns1 <xhrl233@naver.com>
2025-07-27 00:36:04 +0900

    feat:@ConstructorBinding 사용

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -2,11 +2,13 @@ package gift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

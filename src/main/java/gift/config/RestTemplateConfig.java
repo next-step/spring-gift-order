fdade1328a5a15c5a949e86c20dfa041809e17b2
uvrvuoon <0524yar@gmail.com>
2025-07-27 21:21:49 +0900

    feat: RestTemplate에 타임아웃 설정 적용

--- src/main/java/gift/config/RestTemplateConfig.java
+++ src/main/java/gift/config/RestTemplateConfig.java
@@ -1,5 +1,7 @@
 package gift.config;
 
+import java.time.Duration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +11,11 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .build();
+
+        return restTemplate;
     }
 }

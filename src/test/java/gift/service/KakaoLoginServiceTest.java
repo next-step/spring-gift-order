a72915eb15e9de6f796850c64626c2012678c23e
uvrvuoon <0524yar@gmail.com>
2025-07-27 21:37:44 +0900

    feat: KakaoLoginService의 getAccessToken 메서드에 대한 단위 테스트 작성

---
+++ src/test/java/gift/service/KakaoLoginServiceTest.java
@@ -0,0 +1,50 @@
+package gift.service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import org.springframework.http.MediaType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest
+public class KakaoLoginServiceTest {
+
+    @Autowired
+    private KakaoLoginService kakaoLoginService;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private MockRestServiceServer mockServer;
+
+    @BeforeEach
+    void setUp() {
+        mockServer = MockRestServiceServer.createServer(restTemplate);
+    }
+
+    @Test
+    void getAccessTokenTest() {
+        // given
+        var code = "codee";
+        var expectedToken = "tokenn";
+        var expectedBody = "{\"access_token\":\"" + expectedToken + "\"}";
+
+        this.mockServer.expect(requestTo("https://kauth.kakao.com/oauth/token"))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withSuccess(expectedBody, MediaType.APPLICATION_JSON));
+
+        // when
+        var token = kakaoLoginService.getAccessToken(code);
+
+        // then
+        assertThat(token).isEqualTo(expectedToken);
+    }
+
+}

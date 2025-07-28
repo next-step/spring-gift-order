203f187ec5d57e80329c58d5d88deb228604a0b1
treasure-sky <ych0486666@gmail.com>
2025-07-28 16:46:16 +0900

    feat: 카카오 로그인 api url 변경

--- src/main/java/gift/kakao/controller/KakaoLoginController.java
+++ src/main/java/gift/kakao/controller/KakaoLoginController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/oauth/kakao")
 public class KakaoLoginController {
 
     private final String clientId;

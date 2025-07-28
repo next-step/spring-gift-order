114905184a1f5a5bb84c707876e5896483a0fe4e
sweet-mine <dltlgos@naver.com>
2025-07-29 00:46:11 +0900

    KakaoAuth용 RestTemplate 정의

--- src/main/java/gift/configuration/AppConfig.java
+++ src/main/java/gift/configuration/AppConfig.java
@@ -1,13 +1,25 @@
 package gift.configuration;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class AppConfig {
+
+    // 카카오 인증 관련 API 요청 템플릿
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate KaKaoAuthRestTemplate(RestTemplateBuilder builder) {
+        return builder
+                .rootUri("https://kauth.kakao.com/oauth")
+                .connectTimeout(Duration.ofSeconds(10))
+                .readTimeout(Duration.ofSeconds(10))
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .build();
     }
 }

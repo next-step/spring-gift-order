071fd934cdfe44a79b93d2300737fe2c0ad9750b
HJH <jijinny26@gmail.com>
2025-07-29 08:05:23 +0900

    feat: order API 기능 추가

---
+++ src/main/java/gift/controller/OrderController.java
@@ -0,0 +1,31 @@
+package gift.controller;
+
+import gift.annotation.LoginMember;
+import gift.dto.OrderRequestDto;
+import gift.dto.OrderResponseDto;
+import gift.entity.Member;
+import gift.service.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderResponseDto> createOrder(@Valid @RequestBody OrderRequestDto dto, @LoginMember Member member) {
+        OrderResponseDto response = orderService.createOrder(member.getId(), dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}

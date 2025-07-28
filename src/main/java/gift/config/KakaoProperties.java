1e930f072811227d1828341c1c9361e6fc85afa7
BSY3510 <baeksy3510@gmail.com>
2025-07-29 02:31:40 +0900

    refactor: apply record in KakaoProperties

--- src/main/java/gift/config/KakaoProperties.java
+++ src/main/java/gift/config/KakaoProperties.java
@@ -1,39 +1,12 @@
 package gift.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableConfigurationProperties
 @ConfigurationProperties(prefix = "kakao")
-public class KakaoProperties {
+public record KakaoProperties(
+    String clientId,
+    String redirectUri,
+    String clientSecret
+) {
 
-    private String clientId;
-    private String redirectUri;
-    private String clientSecret;
-
-    public String clientId() {
-        return clientId;
-    }
-
-    public String redirectUri() {
-        return redirectUri;
-    }
-
-    public String clientSecret() {
-        return clientSecret;
-    }
-
-    public void setClientId(String clientId) {
-        this.clientId = clientId;
-    }
-
-    public void setRedirectUri(String redirectUri) {
-        this.redirectUri = redirectUri;
-    }
-
-    public void setClientSecret(String clientSecret) {
-        this.clientSecret = clientSecret;
-    }
 }

ef2e89e0c6e6da8592a098225a0a53e263c44d20
treasure-sky <ych0486666@gmail.com>
2025-07-28 15:57:26 +0900

    feat: 멤버 <-> 카카오토큰 연관관계 설정

--- src/main/java/gift/member/MemberEntity.java
+++ src/main/java/gift/member/MemberEntity.java
@@ -1,10 +1,15 @@
 package gift.member;
 
+import gift.kakao.KakaoTokenEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +28,11 @@ public class MemberEntity {
 
     @Column(name = "password", nullable = false, length = 255)
     private String password;
+
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "kakao_token_id", unique = true)
+    private KakaoTokenEntity kakaoToken;
 
     protected MemberEntity() {
     }

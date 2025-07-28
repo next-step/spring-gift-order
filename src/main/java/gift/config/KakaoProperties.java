7e1f31a5dd9d9b8f32abe4547a8926a9e162dfc2
Hyun <hyun_7586@naver.com>
2025-07-28 11:59:48 +0800

    chore: 사용하지 않는 @Value 어노테이션 제거

--- src/main/java/gift/config/KakaoProperties.java
+++ src/main/java/gift/config/KakaoProperties.java
@@ -1,6 +1,5 @@
 package gift.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "kakao")
 public class KakaoProperties {
 
-    @Value("${kakao.client-id}")
     private String clientId;
-    @Value("${kakao.redirect-uri}")
     private String redirectUri;
 
     public String getClientId() {

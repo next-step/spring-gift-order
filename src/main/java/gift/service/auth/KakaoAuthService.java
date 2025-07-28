49c07ba4a675924f4366e4633201dca15d399253
Hyun <hyun_7586@naver.com>
2025-07-28 11:29:14 +0800

    chore: unused import문 제거

--- src/main/java/gift/service/auth/KakaoAuthService.java
+++ src/main/java/gift/service/auth/KakaoAuthService.java
@@ -1,16 +1,11 @@
 package gift.service.auth;
 
-import gift.config.KakaoProperties;
-import gift.dto.kakao.KakaoTokenResponse;
 import gift.global.util.KakaoAuthClient;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestClient;
 
 @Service
 public class KakaoAuthService {
+
     private final KakaoAuthClient kakaoAuthClient;
 
     public KakaoAuthService(KakaoAuthClient kakaoAuthClient) {

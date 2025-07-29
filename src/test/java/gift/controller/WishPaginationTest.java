18135a7f822ad7fa2acedc9cfe48de90963fa07f
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-07-29 16:48:04 +0900

    test: 테스트 시 data.sql 충돌 방지를 위한 sql.init.mode 설정 추가

--- src/test/java/gift/controller/WishPaginationTest.java
+++ src/test/java/gift/controller/WishPaginationTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@TestPropertySource(properties = "spring.sql.init.mode=never")
 class WishPaginationTest {
 
     @Autowired private MockMvc mockMvc;

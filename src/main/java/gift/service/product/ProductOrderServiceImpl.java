c5f5c8d6bdab66634057a205dead6b80fbaab44d
raminKim <151427064+raminkim@users.noreply.github.com>
2025-07-29 15:22:51 +0900

    Fix: 상품 주문 시 해당 상품을 위시 리스트에서 삭제하도록 수정

--- src/main/java/gift/service/product/ProductOrderServiceImpl.java
+++ src/main/java/gift/service/product/ProductOrderServiceImpl.java
@@ -5,12 +5,13 @@ import gift.dto.order.KakaoOrderResponseDto;
 import gift.entity.Member;
 import gift.entity.Product;
 import gift.entity.ProductOption;
+import gift.entity.Wish;
 import gift.exception.KakaoSendMessageException;
 import gift.exception.ResourceNotFoundException;
 import gift.repository.member.MemberRepository;
 import gift.repository.product.ProductOptionRepository;
 import gift.repository.product.ProductRepository;
-import java.util.List;
+import gift.repository.wishlist.WishListRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,24 +21,30 @@ public class ProductOrderServiceImpl implements ProductOrderService {
     private final ProductRepository productRepository;
     private final ProductOptionRepository productOptionRepository;
     private final MemberRepository memberRepository;
+    private final WishListRepository wishListRepository;
     private final KakaoClient kakaoClient;
 
     public ProductOrderServiceImpl(ProductRepository productRepository,
         ProductOptionRepository productOptionRepository, MemberRepository memberRepository,
+        WishListRepository wishListRepository,
         KakaoClient kakaoClient) {
         this.productRepository = productRepository;
         this.productOptionRepository = productOptionRepository;
         this.memberRepository = memberRepository;
+        this.wishListRepository = wishListRepository;
         this.kakaoClient = kakaoClient;
     }
 
     @Override
     @Transactional
-    public KakaoOrderResponseDto sendOrderMessage(Long productId, Long productOptionId, Long memberId, String message,
+    public KakaoOrderResponseDto sendOrderMessage(Long productId, Long productOptionId,
+        Long memberId, String message,
         int quantity) {
         Product product = productRepository.findById(productId)
             .orElseThrow(() -> new ResourceNotFoundException());
         Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new ResourceNotFoundException());
+        Wish wish = wishListRepository.findByProductIdAndMemberId(productId, memberId)
             .orElseThrow(() -> new ResourceNotFoundException());
 
         KakaoOrderResponseDto kakaoOrderResponseDto = kakaoClient.sendKakaoMessage(
@@ -52,6 +59,8 @@ public class ProductOrderServiceImpl implements ProductOrderService {
             .orElseThrow(() -> new ResourceNotFoundException());
 
         productOption.decreaseQuantity(quantity);
+
+        wishListRepository.delete(wish);
 
         return kakaoOrderResponseDto;
     }

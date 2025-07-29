b5e8058a73bf39a2c84357728944a4953e0e7c05
BSY3510 <baeksy3510@gmail.com>
2025-07-29 17:19:59 +0900

    feat(repo): add deleteByProduct for Order

--- src/main/java/gift/repository/WishItemRepository.java
+++ src/main/java/gift/repository/WishItemRepository.java
@@ -1,5 +1,6 @@
 package gift.repository;
 
+import gift.entity.Product;
 import gift.entity.WishItem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface WishItemRepository extends JpaRepository<WishItem, Long> {
 
     Page<WishItem> findAllByMemberId(Long memberId, Pageable pageable);
+
+    void deleteByProduct(Product product);
 }

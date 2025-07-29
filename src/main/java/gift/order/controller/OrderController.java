ea49ed1c77df2b2d123944362ceab53f46597caf
treasure-sky <ych0486666@gmail.com>
2025-07-29 16:11:22 +0900

    feat: 주문 삭제 기능 삭제

--- src/main/java/gift/order/controller/OrderController.java
+++ src/main/java/gift/order/controller/OrderController.java
@@ -9,9 +9,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,15 +40,6 @@ public class OrderController {
     ) {
         List<OrderResponseDto> orderResponseDtos = orderService.getAll(member.id());
         return ResponseEntity.ok(orderResponseDtos);
-    }
-
-    @DeleteMapping("/{orderId}")
-    public ResponseEntity<Void> deleteOrder(
-        @PathVariable Long orderId,
-        @LoginMember AuthenticatedMember member
-    ) {
-        orderService.delete(orderId, member.id());
-        return ResponseEntity.noContent().build();
     }
 
 }

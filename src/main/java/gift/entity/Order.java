97a2f75f45b7829078be08ccbbab2f1feadd1cec
BSY3510 <baeksy3510@gmail.com>
2025-07-29 17:14:50 +0900

    feat(entity): add Order entity

---
+++ src/main/java/gift/entity/Order.java
@@ -0,0 +1,60 @@
+package gift.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long optionId;
+    private Integer quantity;
+    private LocalDateTime orderDateTime;
+    private String message;
+
+    protected Order() {
+    }
+
+    public Order(Long id, Long optionId, Integer quantity, LocalDateTime orderDateTime,
+        String message) {
+        this.id = id;
+        this.optionId = optionId;
+        this.quantity = quantity;
+        this.orderDateTime = orderDateTime;
+        this.message = message;
+    }
+
+    public Order(Long optionId, Integer quantity, LocalDateTime orderDateTime,
+        String message) {
+        this(null, optionId, quantity, orderDateTime, message);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getOptionId() {
+        return optionId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public LocalDateTime getOrderDateTime() {
+        return orderDateTime;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}

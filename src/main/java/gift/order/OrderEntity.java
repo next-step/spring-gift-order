90a9b614f024682f40b5d2c654f8245f487a1ef0
treasure-sky <ych0486666@gmail.com>
2025-07-29 17:52:46 +0900

    fix: order 테이블 이름 변경

--- src/main/java/gift/order/OrderEntity.java
+++ src/main/java/gift/order/OrderEntity.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
-@Table(name = "order")
+@Table(name = "orders")
 public class OrderEntity {
 
     @Id

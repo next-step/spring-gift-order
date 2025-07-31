34e33c440f142d10f52c4d20fd5289d4504c7e4f
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-07-31 21:33:49 +0900

    Feat: Kakao 고유 ID 와 LoginProviderType 에 따라 삭제

--- src/main/java/gift/user/repository/UserRepository.java
+++ src/main/java/gift/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package gift.user.repository;
 
+import gift.shared.domain.LoginProviderType;
 import gift.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,10 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
     @Query("SELECT u FROM User u WHERE u.email=:email ")
     Optional<User> findByEmail(@Param("email") String email);
+
+    @Query("SELECT u FROM User u WHERE u.oauthId=:oauthId AND u.providerType=:providerType")
+    Optional<User> findByOAuthIdAndProviderType(
+            @Param("oauthId") Long oauthId,
+            @Param("providerType") LoginProviderType providerType
+    );
 }

86cfbe740e0f7dd0459f6e9e7e68c7cf33cc9e5c
xodbs126 <dltvmsskd@naver.com>
2025-08-01 14:41:47 +0900

    feat: cors 설정 추가

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -37,4 +37,16 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor((new TemporaryUserCheckInterceptor(tokenUtils)))
                 .addPathPatterns("/**");
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Location")
+                .allowCredentials(true)
+                .maxAge(1800);
+    }
+
 }

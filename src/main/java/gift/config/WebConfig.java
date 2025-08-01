d8d1a8c6cd29c6a1a1bf78fff566bc0b1a3e4314
No_way <74843137+thdgmlwns1@users.noreply.github.com>
2025-08-01 14:15:52 +0900

    경북대_송희준_Step3 (#245)
    * feat: test 코드와 cors 설정
    
    * docs: README 작성
    
    * Update README.md

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -2,7 +2,9 @@ package gift.config;
 
 import gift.interceptor.AdminCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -31,5 +33,18 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(resolver);
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    public static final String[] ALLOWED_METHOD_NAMES = {
+            "GET", "POST", "PUT", "DELETE", "OPTIONS"
+    };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")  // 모든 Origin 허용
+                .allowedMethods(ALLOWED_METHOD_NAMES)
+                .exposedHeaders(HttpHeaders.LOCATION) // Location 헤더 노출
+                .allowCredentials(false);
     }
 }

18e6aed41d02476a111792c1860511519ced8144
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-08-01 19:18:59 +0900

    Feat: Shell 변경

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,28 @@
 
 GIT_URL=https://github.com/Sunja-An/spring-gift-order.git
 GIT_BRANCH_NAME=sunja-an
+PROJECT_PATH=/home/ubuntu/spring-gift-order
 
-BUILD_PATH=$(ls /home/ubuntu/deploy/app.jar)
+echo "> 🚀 Deploy Start"
+
+cd $PROJECT_PATH
+
+echo "> 🔨 Build Start"
+./gradlew clean build -x test
+
+LOG_PATH=$PROJECT_PATH/logs
+mkdir -p $LOG_PATH
+
+# -plain.jar 제외하고 가장 최근 jar 선택
+BUILD_PATH=$(ls $PROJECT_PATH/build/libs/spring-gift-*.jar | grep -v 'plain' | head -n1)
 JAR_NAME=$(basename $BUILD_PATH)
 
 echo "> 🚀 NEW APPLICATION: $JAR_NAME"
 
 LOG_FILE="/home/ubuntu/app.log"
 
-CURRENT_PID=$(pgrep -f $JAR_NAME)
-
+# 현재 실행 중인 프로세스 종료
+CURRENT_PID=$(pgrep -f "java.*$JAR_NAME")
 if [ -z "$CURRENT_PID" ]; then
     echo "> NO APPLICATION 🥲"
 else
@@ -20,12 +32,14 @@ else
     sleep 5
 fi
 
+# 배포
 DEPLOY_PATH=/home/ubuntu/
 cp $BUILD_PATH $DEPLOY_PATH
 cd $DEPLOY_PATH
 
 echo "> 📚 새 애플리케이션 배포"
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+DEPLOY_JAR=$DEPLOY_PATH/$JAR_NAME
+
 nohup java -jar -Dspring.profiles.active=prod $DEPLOY_JAR > $LOG_FILE 2>&1 &
 
 echo "> 🚀 배포 완료"

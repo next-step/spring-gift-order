24c19753ad903cdf6928ae2acdff25837c8e9f38
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2025-08-02 09:14:23 +0900

    강원대 BE 홍지환 step3 - 주문하기 (#307)
    * feat: CORS 설정
    
    * fix: jwt secret key 위치 변경(application-secret) 및 키 변경
    
    * fix: 토큰 만료시간 수정
    
    * feat: deploy script 추가
    
    * feat: redirect-uri 수정
    
    * feat: deploy sudo 권한 체크
    
    * fix: typo
    
    * fix: bug fix and add log file
    
    * docs: update README.md

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root or use sudo."
+  exit 1
+fi
+
+APP_PATH=$(pwd)
+BUILD_PATH="${APP_PATH}/build/libs"
+GIT_REMOTE=origin
+GIT_BRANCH=flareseek
+APP_PORT=80
+
+function print_log() {
+  echo "=================================================="
+  echo "[DEPLOY] $1"
+}
+
+print_log "Fetching files from Github"
+git pull ${GIT_REMOTE} ${GIT_BRANCH}
+
+print_log "Building the application..."
+./gradlew clean build
+
+JAR_NAME=$(ls ${BUILD_PATH} | grep .jar$ | grep -v "plain")
+JAR_PATH="${BUILD_PATH}/${JAR_NAME}"
+
+print_log "Stopping the application on port ${APP_PORT}"
+PID=$(lsof -i :${APP_PORT} | grep java | awk '{print $2}')
+if [ -n "$PID" ]; then
+  print_log "killing process with PID: ${PID}"
+  kill -9 $PID
+fi
+
+if [ -f "$JAR_PATH" ]; then
+  print_log "Starting new application..."
+  nohup java -jar -Dserver.port=${APP_PORT} "$JAR_PATH" > log.log 2>&1 &
+  print_log "Deployment successful."
+else
+  print_log "Build failed. JAR file not found at $JAR_PATH"
+  exit 1
+fi
+
+

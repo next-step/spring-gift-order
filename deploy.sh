2603c6e6922fcc7ff62dbed5e3b405ce5e52af20
Sunwoo-An <fellin1919@knu.ac.kr>
2025-08-03 15:48:34 +0900

    경북대 BE 안선우 Step3 배포하기 (#327)
    * Feat: 코드 가져오기
    
    * Feat: 설정 파일 및 gitignore 업데이트
    
    * Feat: README.md update
    
    * Feat: Kakao Login 로직 구현
    
    * Feat: Unused Field 삭제
    
    * Feat: Unused Class 삭제
    
    * Feat: 비밀번호 암호화 Exception Handler 추가
    
    * Feat: RestClient Bean 설정
    
    * Feat: 상수 값 status 로 설정
    
    * Feat: Email 값으로 User 객체 생성 로직 추가
    
    * Feat: Kakao 응답값 response DTO 설정
    
    * Refactor: 중복 로직 수정 및 역할에 따른 Method 분배
    
    * Feat: Kakao Login 을 위한 Column 추가
    
    * Feat: Random Password Generator
    
    * Feat: Kakao Login Token용 Annotation 설정
    
    * Feat: 자신에게 보내기 URL 설정
    
    * Feat: Order Entity 설정 및 Layered Architecture 설정
    
    * Feat: Option 커스텀 예외 설정
    
    * Feat: Option 커스텀 예외 설정
    
    * Feat: Login Domain 설정
    
    * Feat: Option Entity 수정 및 정적 정보 Status 로 따로 설정
    
    * Feat: KakaoLogin Argument Handler 설정
    
    * Feat: 상품에 따른 Wishlist 삭제 로직 구현
    
    * Feat: Kakao 고유 ID 와 LoginProviderType 에 따라 삭제
    
    * Feat: Kakao 자신에게 보내기 Logic 설정
    
    * Fix: Build Error 임시해결
    
    * Feat: Web MVC Url 추가
    
    * Feat: deploy shell 추가
    
    * Feat: port 선언
    
    * Feat: Shell 변경
    
    * Feat: MySQL Driver dependency added
    
    * Feat: Feedback 반영
    
    * Feat: Deploy shell 변경
    
    * Feat: Cors 해결

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+GIT_URL=https://github.com/Sunja-An/spring-gift-order.git
+GIT_BRANCH_NAME=sunja-an
+PROJECT_PATH=/home/ubuntu/spring-gift-order
+
+echo "> 🚀 Deploy Start"
+
+cd $PROJECT_PATH
+
+echo "> 🔨 Build Start"
+./gradlew clean build -x test
+
+LOG_PATH=$PROJECT_PATH/logs
+mkdir -p $LOG_PATH
+
+BUILD_PATH=$(ls $PROJECT_PATH/build/libs/spring-gift-*.jar | grep -v 'plain' | head -n1)
+JAR_NAME=$(basename $BUILD_PATH)
+
+echo "> 🚀 NEW APPLICATION: $JAR_NAME"
+
+LOG_FILE="/home/ubuntu/app.log"
+
+CURRENT_PID=$(pgrep -f "java.*$JAR_NAME")
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> NO APPLICATION 🥲"
+else
+    echo "> 🙇 QUIT APPLICATION (PID: $CURRENT_PID)"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+DEPLOY_PATH=/home/ubuntu/
+cp $BUILD_PATH $DEPLOY_PATH
+cd $DEPLOY_PATH
+
+echo "> 📚 새 애플리케이션 배포"
+DEPLOY_JAR=$DEPLOY_PATH/$JAR_NAME
+nohup java -jar -Dspring.profiles.active=prod $DEPLOY_JAR > $LOG_FILE 2>&1 &
+
+echo "> 🚀 배포 완료"
--- src/main/java/gift/shared/config/WebMvcConfig.java
+++ src/main/java/gift/shared/config/WebMvcConfig.java
@@ -4,8 +4,10 @@ import gift.shared.filter.KakaoLoginMemberArgumentHandler;
 import gift.shared.filter.LoginMemberArgumentHandler;
 import gift.shared.token.service.TokenService;
 import gift.user.service.UserService;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -24,5 +26,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentHandler(userService, tokenService));
         resolvers.add(new KakaoLoginMemberArgumentHandler());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name())
+                .allowCredentials(true)
+                .maxAge(1800);
     }
 }
